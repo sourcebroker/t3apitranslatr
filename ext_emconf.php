838dadfa249a14163a302a27c5f237e5f0b66aa5
Krystian Szymukowicz <k.szymukowicz@gmail.com>
2020-02-14 14:33:18 +0100

    Fix naming issue.

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,7 +2,7 @@
 /** @var string $_EXTKEY */
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Language labels as JSON',
-    'description' => 'Connector between ext:t3api and ext:translator which allows to get language labels as JSON.',
+    'description' => 'Connector between ext:t3api and ext:translatr which allows to get language labels as JSON.',
     'category' => 'module',
     'author' => '',
     'author_email' => '',
